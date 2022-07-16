f863ee690061d3b24cf7d8bae6a8b96fb8a0f716
Tadashi-hue <remagnavana@gmail.com>
2022-07-16 15:21:16 +0100

    first push

---
+++ src/axios.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+const axiosInstance = axios.create(
+    {
+        baseURL:'https://asset-manager-app.herokuapp.com'
+    }
+)
+
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) =>
+        Promise.reject(
+            (error.response && error.response.data) || 'Something went wrong!'
+        )
+)
+
+export default axiosInstance
